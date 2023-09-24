9bf76ce2d559d4d4790b8f800139e117d431ad64
sandeepkumar565 <sandeepkumar10091999@gmail.com>
2023-09-24 23:07:34 +0530

    move files to public folder

--- public/index.js
+++ public/index.js
@@ -1,34 +1,32 @@
-let b = document.getElementById("button")
-b.onclick = weatherInfo
+let b = document.getElementById("button");
+b.onclick = weatherInfo;
 
 // Automatic weather updates
-let cityarr = ["New Delhi", "Dubai", "Istanbul", "Paris", "London", "New York", "Tokyo", "Sydney", "Shanghai", "Singapore"]
-let interval = setInterval(weatherAuto, 2000);
+let cityarr = ["New Delhi", "Dubai", "Istanbul", "Paris", "London", "New York", "Tokyo", "Sydney", "Shanghai", "Singapore"];
+let interval = setInterval(weatherAuto, 4000);
 
+let cityIndex = 0;
 function weatherAuto() {
-    let d = new Date();
-    let sec = d.getSeconds();
-    let city = cityarr[sec % 10];
+    cityIndex = (++cityIndex) % 10
 
     let api = "82796fcf91ded29a924e178e5c16a341";
-    let url = 'http://api.openweathermap.org/data/2.5/weather?' + 'q=' + city + '&APPID=' + api + "&units=metric"
+    let url = 'https://api.openweathermap.org/data/2.5/weather?' + 'q=' + cityarr[cityIndex]+ '&APPID=' + api + "&units=metric"
 
     fetch(url).then(function(resp) {
         return resp.json()
     }).then(function(data) {
-        //console.log(data)
 
         let weather = data;
         let h11 = document.getElementById("h11");
         let mausam1 = document.getElementById("mausam1");
 
-        h11.innerHTML = "Weather updates of   " + city;
+        h11.innerHTML = "Weather updates of   " + cityarr[cityIndex];
 
         mausam1.innerHTML =
             "Temperature : " + weather.main.temp + " °C<br>" + "Humidity : " + weather.main.humidity + " %<br>" +
             "Pressure : " + weather.main.pressure + " hpa<br>" + "Wind Speed : " + weather.wind.speed + " m/s";
 
-    }).catch()
+    }).catch(err => console.error('an error ocurred', err))
 }
 
 // weather update of desired city
@@ -67,7 +65,3 @@ document.getElementById('listen').addEventListener('click', function() {
       alert('Text-to-speech not supported.');
     }
 });
-
-//let city = window.prompt("Enter city : ");
-
-//console.log(weather.main.temp)
